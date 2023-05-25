4cb4ecbed93a323c4cb8ff3e5225d80e326e6a16
Tomas Figueroa <figueroalopeztomas@gmail.com>
2023-05-25 15:49:18 -0300

    Realizando modificacion clase controller

--- Controllers/GestorRegistroDeRespuestaController.cs
+++ Controllers/GestorRegistroDeRespuestaController.cs
@@ -4,10 +4,11 @@ using dsi_ppai_ivr_g8.Entities;
 namespace dsi_ppai_ivr_g8.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("[controller]")]
 public class GestorRegistroDeRespuestaController : ControllerBase
 {
-    private List<Llamada> llamadas = new List<Llamada> {
+    private List<Llamada> llamadas = new List<Llamada>
+    {
         /*
         new Llamada("tomas1"),
         new Llamada("tomas2"),
@@ -25,7 +26,13 @@ public class GestorRegistroDeRespuestaController : ControllerBase
     {
         return buscarLlamada();
     }
-    
+
+    [NonAction]
+    public void tomarOpcionOperador()
+    {
+
+    }
+
     [NonAction]
     public Llamada buscarLlamada()
     {
@@ -34,10 +41,88 @@ public class GestorRegistroDeRespuestaController : ControllerBase
 
         return llamadas[index];
     }
-    
+
+    [NonAction]
+    public void asignarEstadoEnCurso()
+    {
+
+    }
+
     [NonAction]
     public string getFechaHoraActual()
     {
         return DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+    }
+
+    [NonAction]
+    public void buscarValidacionesSubOpcion()
+    {
+
+    }
+
+    [NonAction]
+    public void ordenarValidacion()
+    {
+
+    }
+
+    [NonAction]
+    public void ingresarRespuestasValidaciones()
+    {
+
+    }
+
+    [NonAction]
+    public void tomarRespuestaValidacion()
+    {
+
+    }
+
+    [NonAction]
+    public void validarRespuestaIngresada()
+    {
+
+    }
+
+    [NonAction]
+    public void ingresarDescripcionRespuesta()
+    {
+
+    }
+
+    [NonAction]
+    public void tomarDescripcionRespuesta()
+    {
+
+    }
+
+    [NonAction]
+    public void confirmarOperacion()
+    {
+
+    }
+
+    [NonAction]
+    public void registrarAccion()
+    {
+
+    }
+
+    [NonAction]
+    public void registrarFinalizacionLlamada()
+    {
+
+    }
+
+    [NonAction]
+    public void asignarEstadoFinalizado()
+    {
+
+    }
+
+    [NonAction]
+    public void finCU()
+    {
+
     }
 }
